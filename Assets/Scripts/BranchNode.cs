9cfe6dfa8f46ff826444187d11cdc6e318cbc184
BATzerk <bretttaylor@Bretts-MacBook-Air.local>
2016-01-30 13:40:44 -0500

    +Fractal-like node generation!

--- Assets/Scripts/BranchNode.cs
+++ Assets/Scripts/BranchNode.cs
@@ -10,26 +10,30 @@ public class BranchNode : MonoBehaviour {
 
 
 	// Constructor
-	public void Initialize (Vector2 _pos, BranchNode _previousNode, Vector2 sourcePos, float maxDistanceFromSourcePos) {
+	public void Initialize (Vector2 _pos, BranchNode _previousNode, Vector2 sourcePos, float maxDistanceFromSourcePos, int numNodesCreated) {
 		pos = _pos;
 		this.transform.localPosition = pos;
 		previousNode = _previousNode;
 		nextNodes = new List<BranchNode> ();
 		this.gameObject.name = "BranchNode";
 		
-		MakeNextNodeAttempt (sourcePos, maxDistanceFromSourcePos);
-//		MakeNextNodeAttempt (sourcePos, maxDistanceFromSourcePos);
+		MakeNextNodeAttempt (sourcePos, maxDistanceFromSourcePos, numNodesCreated, true);
+		MakeNextNodeAttempt (sourcePos, maxDistanceFromSourcePos, numNodesCreated, false);
 	}
 
 
-	private void MakeNextNodeAttempt(Vector2 sourcePos, float maxDistanceFromSourcePos) {
-		float angleFromPreviousNode = 0;
+	private void MakeNextNodeAttempt(Vector2 sourcePos, float maxDistanceFromSourcePos, int numNodesCreated, bool isLeftTurn) {
+		// Created too many nodes? Get outta town!!
+		if (numNodesCreated > 3) { return; }
+
+		float angleFromPreviousNode = -Mathf.PI*0.5f; // Default to down.
 		if (previousNode != null) {
-			angleFromPreviousNode = Mathf.Atan2 (previousNode.pos.y-pos.y, previousNode.pos.x-pos.x);
+			angleFromPreviousNode = Mathf.Atan2 (pos.y-previousNode.pos.y, pos.x-previousNode.pos.x);
 		}
 
-		float angleToNextNode = 0.1f;//angleFromPreviousNode + Random.Range (-20f, 20f)*Mathf.Deg2Rad;
-		float distanceToNextNode = 20;//Random.Range (400, 800);
+		float differenceVolume = Random.Range (0.5f, 1.5f);
+		float distanceToNextNode = 3 * differenceVolume;
+		float angleToNextNode = angleFromPreviousNode + (isLeftTurn ? -0.5f : 0.5f) * differenceVolume;
 		Vector2 nextNodePos = pos + new Vector2 (Mathf.Cos (angleToNextNode)*distanceToNextNode, Mathf.Sin (angleToNextNode)*distanceToNextNode);
 
 		// If the distance we want to make the next node(s) is too great, DON'T make a next node!!
@@ -39,15 +43,18 @@ public class BranchNode : MonoBehaviour {
 
 		// Make the next node!!
 		BranchNode newNode = new GameObject ().AddComponent<BranchNode> ();
-		newNode.Initialize (nextNodePos, this, sourcePos, maxDistanceFromSourcePos);
+		newNode.Initialize (nextNodePos, this, sourcePos, maxDistanceFromSourcePos, numNodesCreated+1);
 		nextNodes.Add (newNode);
 	}
 
 
 
 	private void OnDrawGizmos () {
-		Gizmos.color = Color.blue;
-		Gizmos.DrawSphere (this.transform.localPosition, 0.5f);
+		Gizmos.color = Color.white;
+		Gizmos.DrawSphere (this.transform.localPosition, 0.1f);
+		if (previousNode != null) {
+			Gizmos.DrawLine (pos, previousNode.pos);
+		}
 	}
 
 
--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -14,9 +14,29 @@ public class WorldGenerator : MonoBehaviour {
 	private void GenerateWorld() {
 		// -- Step 1: Make nodes --
 		BranchNode sourceNode = new GameObject ().AddComponent<BranchNode> ();
-		sourceNode.Initialize (Vector2.zero, null, Vector2.zero, 2000);
+		sourceNode.Initialize (Vector2.zero, null, Vector2.zero, 2000, 0);
 
 
+	}
+
+
+	private void Update() {
+		AcceptButtonInput ();
+	}
+
+	private void AcceptButtonInput() {
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			DestroyAllNodes();
+			GenerateWorld();
+		}
+	}
+
+
+	private void DestroyAllNodes() {
+		BranchNode[] allNodes = GameObject.FindObjectsOfType<BranchNode> ();
+		for (int i=0; i<allNodes.Length; i++) {
+			Destroy (allNodes[i].gameObject);
+		}
 	}
 
 }
